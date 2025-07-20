990b0b68e3a9d3a062e575dc2bc5d8a8c2e794a8
Danitha Dinuwan <danithahk@gmail.com>
2025-07-20 14:54:58 +0530

    create state

---
+++ src/model/bookData.ts
@@ -0,0 +1,9 @@
+export interface bookData {
+    title: string;
+    author: string;
+    description: string;
+    price: number;
+    quantity: number;
+    category: string;
+    imageUrl: string;
+}
