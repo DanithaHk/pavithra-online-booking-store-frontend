6ece067cd56be1297cafa8bd7ae30a32c70e2768
Danitha Dinuwan <danithahk@gmail.com>
2025-07-21 20:54:02 +0530

    done  home page

--- src/view/pages/Home.tsx
+++ src/view/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from "./component/Banner.tsx";
 import News from "./component/News.tsx";
+import Label from "./component/Label.tsx";
 
 export function Home() {
     return (
         <>
             <Banner/>
             <News/>
+            <Label/>
         </>
 
     );
